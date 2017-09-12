05959a1ed767a82a74c1251dd4187c24363bcbca
amberdas <kumaramber2@gmail.com>
2017-09-13 00:21:19 +0530

    Update Decryptor.java

--- Decryptor.java
+++ Decryptor.java
@@ -1,9 +1,5 @@
 package security.practice;
 
-/*Abstraction Layer where we define the design of Decryptor
- * This means that any decryptor must have textdecryptor method which will
- * take byte array as argument to decrypt it*/
-
 public interface Decryptor 
 {
 	String getDecryptedText();
