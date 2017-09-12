b02ea992399c1ddbccc59cf2f6661b93024ff9bc
amberdas <kumaramber2@gmail.com>
2017-09-13 00:23:29 +0530

    Update Encryptor.java

--- Encryptor.java
+++ Encryptor.java
@@ -1,9 +1,5 @@
 package security.practice;
 
-/*Abstraction Layer where we define the design of Encryptor
- * This means that any encryptor must have textencryptor method which will
- * take byte array as argument to encrypt it*/
-
 public interface Encryptor 
 {
 	byte[] getEncryptedText();
